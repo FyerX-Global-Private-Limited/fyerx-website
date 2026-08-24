3cdfe1521b15dfb92b3d0054d79bd82b3b2e0fdc
sivaranjani-expertspro <sivaranjani.j@expertspro.io>
2026-08-24 16:43:05 +0530

    changesdone

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export const viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
+  colorScheme: "light",
 };
 
 export default function RootLayout({
@@ -49,9 +50,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${interTight.variable} ${inter.variable} ${poppins.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${interTight.variable} ${inter.variable} ${poppins.variable} h-full antialiased light`}
+      style={{ colorScheme: "light" }}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-white text-[#171717]">
         <StyledJsxRegistry>{children}</StyledJsxRegistry>
       </body>
     </html>
